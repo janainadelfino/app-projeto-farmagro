03f25e10004057cc15a0608f3012243e9f4ca4a3
Janaina <60708545+janaina-souza-delfino@users.noreply.github.com>
2022-10-24 20:09:39 -0300

    Revert "Consumindo API"

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { Button, useTheme, View } from "native-base";
+import { useTheme } from "native-base";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { Home } from "../screens/Home/";
+import { StackRoutes } from "./StackRoutes";
 
 const Drawer = createDrawerNavigator();
 
@@ -12,31 +12,10 @@ export function Routes() {
     <Drawer.Navigator>
       <Drawer.Screen
         name="Home"
-        component={Home}
+        component={StackRoutes}
         options={{
           drawerIcon: ({ focused, size, color }) => (
             <MaterialCommunityIcons name="home" size={size} color={color} />
-          ),
-          title: " ",
-        }}
-      />
-
-      <Drawer.Screen
-        name="Favoritos"
-        component={Home}
-        options={{
-          drawerIcon: ({ focused, size, color }) => (
-            <MaterialCommunityIcons name="heart" size={size} color={color} />
-          ),
-        }}
-      />
-
-      <Drawer.Screen
-        name="HistÃ³rico"
-        component={Home}
-        options={{
-          drawerIcon: ({ focused, size, color }) => (
-            <MaterialCommunityIcons name="book" size={size} color={color} />
           ),
         }}
       />
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -15,32 +15,19 @@ import {
   Container,
 } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
-import { Api } from "../services/api";
-import { useEffect, useState } from "react";
 
 export function Home() {
-  const [planta, setPlanta] = useState([]);
-
-  async function fetchDados() {
-    await Api.get("/planta/")
-      .then((response) => setPlanta(response.data))
-      .catch((error) => alert(error));
-  }
-
-  useEffect(() => {
-    fetchDados();
-  }, []);
-
   return (
     <VStack flex={1} mb={10} bg="#FFFFFF">
       <VStack>
-        <Center py="-2">
+        <Center py="8">
           <Image
             source={{
               uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDV6XWKl0A-rWwoqzNNCH_VkUdojQ4jIU41w&usqp=CAU",
             }}
-            size="md"
-            w="10%"
+            size="xl"
+            w="38"
+            h="38"
             alt="Imagem da logo"
           />
           <Heading size={"xs"}>AgroTech</Heading>
@@ -52,7 +39,8 @@ export function Home() {
         alignSelf="center"
         my="4"
         space={5}
-        w="85%"
+        w="100%"
+        maxW="300px"
         divider={
           <Box px="2">
             <Divider />
@@ -63,8 +51,9 @@ export function Home() {
           <Input
             placeholder="Pesquisar"
             variant="filled"
+            width="100%"
             borderRadius="10"
-            py="2"
+            py="-2"
             px="2"
             InputLeftElement={
               <Icon
@@ -78,72 +67,174 @@ export function Home() {
         </VStack>
       </VStack>
 
-      <VStack alignItems="center">
-        <Stack direction="row" mb="2" mt="2">
-          {planta.map((plant) => (
+     <VStack alignItems="center">
+      <Stack direction="row" mb="2.5" mt="2" space={1}>
+        <Box px="5">
+          <Box rounded="lg" overflow="hidden">
             <Box>
-              <Box rounded="lg" overflow="hidden">
-                <Box px="8">
-                  <AspectRatio w="100" h="100" rounded="lg" overflow="hidden">
-                    <Image
-                      source={{
-                        uri: "https://images.tcdn.com.br/img/img_prod/799330/erva_cidreira_melissa_459_1_20200523155701.jpg",
-                      }}
-                      alt="image"
-                    />
-                  </AspectRatio>
-                </Box>
-
-                <Stack>
-                  <Stack>
-                    <Heading
-                      color="white"
-                      position="absolute"
-                      bottom="1"
-                      py="5"
-                      px="16"
-                    >
-                      {plant.nomeCientifico}
-                    </Heading>
-                  </Stack>
-                </Stack>
-              </Box>
+              <AspectRatio w="100" h="100" rounded="lg" overflow="hidden">
+                <Image
+                  source={{
+                    uri: "https://images.tcdn.com.br/img/img_prod/799330/erva_cidreira_melissa_459_1_20200523155701.jpg",
+                  }}
+                  alt="image"
+                />
+              </AspectRatio>
             </Box>
-          ))}
 
-          {planta.map((plant) => (
-            /*2 Bloco */
-            <Box px="5">
-              <Box rounded="lg" overflow="hidden">
-                <Box>
-                  <AspectRatio w="100" h="100" rounded="lg" overflow="hidden">
-                    <Image
-                      source={{
-                        uri: "https://images.tcdn.com.br/img/img_prod/799330/erva_cidreira_melissa_459_1_20200523155701.jpg",
-                      }}
-                      alt="image"
-                    />
-                  </AspectRatio>
-                </Box>
+            <Stack>
+              <Stack>
+                <Heading
+                  color="white"
+                  position="absolute"
+                  bottom="9"
+                  py="3.5"
+                  px="18.5"
+                >
+                  Melissa
+                </Heading>
+              </Stack>
+              <Text
+                fontWeight="300"
+                position="relative"
+                bottom="9"
+                color="white"
+                px="1.5"
+                py="-1.5"
+              >
+                (Melissa officinalis)
+              </Text>
+            </Stack>
+          </Box>
+        </Box>
 
-                <Stack>
-                  <Stack>
-                    <Heading
-                      color="white"
-                      position="absolute"
-                      bottom="4"
-                      py="2"
-                      px="10"
-                    >
-                      {plant.nomeCientifico}
-                    </Heading>
-                  </Stack>
-                </Stack>
-              </Box>
+        <Box px="5">
+        <Box rounded="lg" overflow="hidden">
+          <Box>
+            <AspectRatio w="100" h="100" rounded="lg" overflow="hidden">
+              <Image
+                source={{
+                  uri: "https://images.tcdn.com.br/img/img_prod/799330/erva_cidreira_melissa_459_1_20200523155701.jpg",
+                }}
+                alt="image"
+              />
+            </AspectRatio>
+          </Box>
+
+          <Stack>
+            <Stack>
+              <Heading
+                color="white"
+                position="absolute"
+                bottom="9"
+                py="3.5"
+                px="18.5"
+              >
+                Melissa
+              </Heading>
+            </Stack>
+            <Text
+              fontWeight="300"
+              position="relative"
+              bottom="9"
+              color="white"
+              px="1.5"
+              py="-1.5"
+            >
+              (Melissa officinalis)
+            </Text>
+          </Stack>
+        </Box>
+      </Box>
+            
+      </Stack> 
+     </VStack>
+
+     <VStack alignItems="center">
+      <Stack direction="row" mb="2.5" mt="2" space={1}>
+        <Box px="5">
+          <Box rounded="lg" overflow="hidden">
+            <Box>
+              <AspectRatio w="100" h="100" rounded="lg" overflow="hidden">
+                <Image
+                  source={{
+                    uri: "https://images.tcdn.com.br/img/img_prod/799330/erva_cidreira_melissa_459_1_20200523155701.jpg",
+                  }}
+                  alt="image"
+                />
+              </AspectRatio>
             </Box>
-          ))}
-        </Stack>
-      </VStack>
+
+            <Stack>
+              <Stack>
+                <Heading
+                  color="white"
+                  position="absolute"
+                  bottom="9"
+                  py="3.5"
+                  px="18.5"
+                >
+                  Melissa
+                </Heading>
+              </Stack>
+              <Text
+                fontWeight="300"
+                position="relative"
+                bottom="9"
+                color="white"
+                px="1.5"
+                py="-1.5"
+              >
+                (Melissa officinalis)
+              </Text>
+            </Stack>
+          </Box>
+        </Box>
+
+        <Box px="5">
+        <Box rounded="lg" overflow="hidden">
+          <Box>
+            <AspectRatio w="100" h="100" rounded="lg" overflow="hidden">
+              <Image
+                source={{
+                  uri: "https://images.tcdn.com.br/img/img_prod/799330/erva_cidreira_melissa_459_1_20200523155701.jpg",
+                }}
+                alt="image"
+              />
+            </AspectRatio>
+          </Box>
+
+          <Stack>
+            <Stack>
+              <Heading
+                color="white"
+                position="absolute"
+                bottom="9"
+                py="3.5"
+                px="18.5"
+              >
+                Melissa
+              </Heading>
+            </Stack>
+            <Text
+              fontWeight="300"
+              position="relative"
+              bottom="9"
+              color="white"
+              px="1.5"
+              py="-1.5"
+            >
+              (Melissa officinalis)
+            </Text>
+          </Stack>
+        </Box>
+      </Box>
+            
+      </Stack> 
+     </VStack>            
+      
+
+     
     </VStack>
   );
 }
