1710cb567fbf3706da1fa90652d8de52a74f1d32
Janaina <janaina.delfino@unifio.edu.br>
2022-11-08 20:31:43 -0300

    Pesquisa

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,4 @@
-import {
-  VStack,
-  Input,
-  Icon,
-} from "native-base";
+import { VStack, Input, Icon } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
 import { Api } from "../services/api";
 import { useEffect, useState } from "react";
@@ -11,26 +7,34 @@ import { Header } from "../components/Header";
 export function Home() {
   const [planta, setPlanta] = useState([]);
   const [idimage, setImage] = useState([]);
-
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
   async function fetchDados() {
     await Api.get("/planta/")
       .then((response) => setPlanta(response.data))
       .catch((error) => alert(error));
   }
-
+  async function searchPlanta() {
+    console.log(search);
+    if (search) {
+      await Api.get("/planta/nome/" + search)
+        .then((response) => setSearchResult(response.data))
+        .catch((error) => alert(error));
+    } else {
+      await fetchDados();
+    }
+  }
   useEffect(() => {
     fetchDados();
   }, []);
- 
+  useEffect(() => {
+    console.log("!");
+    searchPlanta();
+  }, [search]);
   return (
     <VStack flex={1} bg="#FFFFFF">
       <Header />
-      <VStack
-        alignSelf="center"
-        my="4"
-        w="full"
-        px={4}
-      >
+      <VStack alignSelf="center" my="4" w="full" px={4}>
         <VStack w="100%" alignSelf="center">
           <Input
             placeholder="Pesquisar"
@@ -38,6 +42,9 @@ export function Home() {
             borderRadius="10"
             py="2"
             px="2"
+            onChange={(e) => {
+              setSearch(e.nativeEvent.text);
+            }}
             InputLeftElement={
               <Icon
                 ml="2"
@@ -51,9 +58,7 @@ export function Home() {
       </VStack>
 
       <VStack flex={1}>
-        <Lista
-          data={planta}
-        />
+        <Lista data={searchResult.length > 0 ? searchResult : planta} />
       </VStack>
     </VStack>
   );
